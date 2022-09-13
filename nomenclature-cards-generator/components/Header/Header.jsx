3ad5ece081b1a7e561336bc45627bea232998ede
Supico Bruno <supico.bruno@gmail.com>
2022-09-13 16:28:16 +0200

    début du projet

---
+++ nomenclature-cards-generator/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from 'assets\logo.svg'
+import { useState } from 'react/cjs/react.production.min'
+
+export default function Header() {
+
+
+    return (
+        <>
+            <Logo />
+            <div>Connexion</div>
+            <div>Inscription</div>
+        </>
+    )
+}
---
+++ nomenclature-cards-generator/components/Tabs/Tabs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Tabs() {
+
+  return (
+    <>
+      <div className="Tab">Nomenclature</div>
+      <div className="Tab">Clip card</div>
+      <div className="Tab">Puzzle</div>
+    </>
+  )
+}
